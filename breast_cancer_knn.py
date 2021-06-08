3598e7ab6b0feb5fc65658421994ab7e891a7626
mei2sad <mei2sad@naver.com>
2021-06-08 21:27:19 +0900

    first commit

---
+++ breast_cancer_knn.py
@@ -0,0 +1,68 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+
+breast_cancer_data = load_breast_cancer()
+k=1
+
+df_data = pd.DataFrame(breast_cancer_data.data)
+df_labels = pd.DataFrame(breast_cancer_data.target)
+
+
+def min_max_normalize(lst):
+    
+
+    normalized = []
+    
+    for value in lst:
+        normalized_num = (value - min(lst)) / (max(lst) - min(lst))
+        normalized.append(normalized_num)
+    
+    return normalized
+
+for x in range(len(df_data.columns)):
+    df_data[x] = min_max_normalize(df_data[x])
+
+
+training_data, validation_data , training_labels, validation_labels = train_test_split(df_data, df_labels, test_size = 0.2, random_state = 42)
+
+import matplotlib.pyplot as plt
+
+k_list = range(1,101)
+accuracies = []
+
+for k in k_list:
+  classifier = KNeighborsClassifier(n_neighbors = k)
+  classifier.fit(training_data, training_labels)
+  accuracies.append(classifier.score(validation_data, validation_labels))
+
+plt.plot(k_list, accuracies)
+plt.xlabel("k")
+plt.ylabel("Validation Accuracy")
+plt.title("Breast Cancer Classifier Accuracy")
+plt.show()
+
+#k값을 1~101까지 반복 후 그래프를 보면 1~20이후는 급격히 떨어짐, 따라서 1~20 사이 값을 자세히 추출
+
+k_list = range(1,20)
+#그래프 부분
+accuracies = []
+
+for k in k_list:
+  classifier = KNeighborsClassifier(n_neighbors = k)
+  classifier.fit(training_data, training_labels)
+  accuracies.append(classifier.score(validation_data, validation_labels))
+
+plt.plot(k_list, accuracies)
+plt.xlabel("k")
+plt.ylabel("Validation Accuracy")
+plt.title("Breast Cancer Classifier Accuracy")
+plt.show()
+#정확도 출력 부분
+for i in range(1,20):
+    classifier = KNeighborsClassifier(n_neighbors = i)
+    classifier.fit(training_data, training_labels.values.ravel())
+    #print(classifier.score(validation_data, validation_labels))
+    print("K=%d"%i)
+    print('KNN Accuracy: {:.2f}%'.format(classifier.score(validation_data, validation_labels) * 100))
---
+++ breast_cancer_mlp.py
@@ -0,0 +1,117 @@
+
+# coding: utf-8
+
+from sklearn.datasets import load_breast_cancer
+
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split, StratifiedKFold, GridSearchCV, cross_val_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, roc_auc_score, classification_report
+from sklearn.preprocessing import Normalizer, MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer, LabelEncoder
+from sklearn.pipeline import Pipeline
+
+
+
+cancer = load_breast_cancer()
+
+X_train, X_test, y_train, y_test = train_test_split(cancer.data,cancer.target, random_state=42, test_size=0.2)
+
+
+
+# 훈련 세트 각 특성의 평균
+
+mean_on_train = X_train.mean(axis=0)
+
+# 훈련 세트 각 특성의 표준 편차
+
+std_on_train = X_train.std(axis=0)
+
+
+
+# 표준 정규분포
+
+X_train_scaled = (X_train-mean_on_train)/std_on_train
+
+X_test_scaled = (X_test-mean_on_train)/std_on_train
+
+
+
+random_state = 42
+mlp_activation = ['identity', 'logistic', 'tanh', 'relu']
+mlp_solver = ['lbfgs', 'sgd', 'adam']
+mlp_max_iter = range(100, 1000, 10000)
+mlp_alpha = [1e-4, 1e-3, 0.01, 0.1, 1]
+preprocess = [Normalizer(), MinMaxScaler(), StandardScaler(), RobustScaler(), QuantileTransformer()]
+
+mlp_param_grid = [
+    {
+        'preprocess': preprocess,
+        'classification__activation': mlp_activation,
+        'classification__solver': mlp_solver,
+        'classification__random_state': [random_state],
+        'classification__max_iter': mlp_max_iter,
+        'classification__alpha': mlp_alpha
+    }
+]
+
+pipe = Pipeline(steps=[
+    ('preprocess', StandardScaler()),
+    ('classification', MLPClassifier())
+])
+
+strat_k_fold = StratifiedKFold(
+     n_splits=10,
+     random_state=42,
+     shuffle=True
+)
+
+mlp_grid = GridSearchCV(
+     pipe,
+     param_grid=mlp_param_grid,
+     cv=strat_k_fold,
+     scoring='f1',
+     n_jobs=-1,
+     verbose=2
+)
+
+mlp_grid.fit(cancer.data, cancer.target)
+
+# Best MLPClassifier parameters
+print(mlp_grid.best_params_)
+# Best score for MLPClassifier with best parameters
+print('\nBest F1 score for MLP: {:.2f}%'.format(mlp_grid.best_score_ * 100))
+
+best_params = mlp_grid.best_params_
+
+scaler = StandardScaler()
+
+print('\nData preprocessing with {scaler}\n'.format(scaler=scaler))
+
+X_train_scaler = scaler.fit_transform(X_train)
+X_test_scaler = scaler.transform(X_test)
+
+mlp = MLPClassifier(
+    max_iter=100,
+    alpha=0.01,
+    activation='logistic',
+    solver='adam',
+    random_state=42
+)
+mlp.fit(X_train_scaler, y_train)
+
+mlp_predict = mlp.predict(X_test_scaler)
+mlp_predict_proba = mlp.predict_proba(X_test_scaler)[:, 1]
+
+print('MLP Accuracy: {:.2f}%'.format(accuracy_score(y_test, mlp_predict) * 100))
+print('MLP AUC: {:.2f}%'.format(roc_auc_score(y_test, mlp_predict_proba) * 100))
+print('MLP Classification report:\n\n', classification_report(y_test, mlp_predict))
+print('MLP Training set score: {:.2f}%'.format(mlp.score(X_train_scaler, y_train) * 100))
+print('MLP Testing set score: {:.2f}%'.format(mlp.score(X_test_scaler, y_test) * 100))
